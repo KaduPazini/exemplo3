44ed0fad547dc992d456c07550226a610e0fece0
KaduPazini <kadupazinigamer@gmail.com>
2026-03-03 21:05:44 -0300

    Add files via upload

---
+++ exemplo3.java
@@ -0,0 +1,8 @@
+public class exemplo3 {
+    public static void main(String[] args) {
+        String nome = "Ana";
+        int idade = 20;
+        System.out.printf("Nome: %-10s Idade: %d%n", nome, idade);
+        
+    }
+}
